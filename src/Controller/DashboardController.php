c8229e3b841b33fc1018f187267bb0980b7e750f
StarBread2 <mikerasell123x@gmail.com>
2025-11-11 19:46:42 +0800

    with vid

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class DashboardController extends AbstractController
+{
+    #[IsGranted ('ROLE_USER')] 
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        // User must have ROLE_USER (checked by the IsGranted attribute)
+        return $this->render('dashboard/index.html.twig');
+    }
+}
